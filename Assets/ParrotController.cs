75c88d5fc82f6fcb9627d677437143ceb1e7d18e
家子智成 <>
2020-07-12 15:48:21 +0900

    移植

---
+++ Assets/ParrotController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParrotController : MonoBehaviour
+{
+    UniGifImage uniGifImage;
+    bool isPlay;
+    void Start()
+    {
+        uniGifImage = GetComponent<UniGifImage>();
+    }
+
+    public void OnClick()
+    {
+        if (isPlay)
+        {
+            uniGifImage.Clear();
+        }
+        else
+        {
+            uniGifImage.Play();
+        }
+        isPlay = !isPlay;
+    }
+}
